1d50f36563a95ac095506620cb9692f32276b928
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-24 16:33:52 -0300

    chore: Trabalhando no envio de email

--- EnvioEmail/views.py
+++ EnvioEmail/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from django.core.mail import BadHeaderError, send_mail
+from django.core.exceptions import ValidationError
+from django.http import HttpResponse
 
-# Create your views here.
+class EmailAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        subject = request.POST.get("subject", "Seu cabeçalho aqui")
+        message = request.POST.get("message", "Sua mensagem aqui")
+        recipient_list = ["martinsbarroskaua@gmail.com"]
+        from_email = "martinsbarroskaua85@gmail.com"
+        try:
+            send_mail(
+                subject,
+                message,
+                recipient_list=recipient_list,
+                from_email=from_email,
+            )
+        except BadHeaderError:
+            return HttpResponse("Cabeçalho inválido!")
+        except ValidationError as e:
+            return HttpResponse(str(e))
+        return HttpResponse(
+            {'Email enviado com sucesso'}, status = status.HTTP_200_OK
+        )
